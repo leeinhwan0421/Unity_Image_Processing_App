a0f3c82b9d438c9636f9a3412d48d831e02abaaf
이인환 <70061182+leeinhwan0421@users.noreply.github.com>
2023-01-10 15:56:50 +0900

    middle commit
    if an assignment is added, i will commit this at future

--- Assets/Scripts/WindowSetting/WindowSetting.cs
+++ Assets/Scripts/WindowSetting/WindowSetting.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class WindowSetting : MonoBehaviour
 {
-    private bool isWindow = true;
-    private int widthPixel = 1280;
-    private int heightPixel = 720;
+    private bool isFullScreen = false;
+    private int widthPixel = 1600;
+    private int heightPixel = 900;
 
     private void Awake()
     {
         SetResolution();
     }
 
-    public void SetResolution() => Screen.SetResolution(widthPixel, heightPixel, isWindow);
+    public void SetResolution() => Screen.SetResolution(widthPixel, heightPixel, isFullScreen);
 
     public void SetResoulution(int width, int height, bool isWindow) => Screen.SetResolution(width, height, isWindow);
 }
